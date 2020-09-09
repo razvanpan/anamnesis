15fe2a3363ac990af6fdb489042ef7726a7ddf13
WHQ_NT_DOMAIN\CB083492 <cata_banu98@yahoo.com>
2020-09-09 12:31:56 +0300

    Create basic structure for anamnesis component

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'anamnesis'
 import 'anamnesis/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Hello world 😄' />
 }
 
 export default App
