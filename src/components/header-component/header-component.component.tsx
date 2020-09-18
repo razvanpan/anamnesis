05032fc53150494945e2cb756eac61772d7e4c2d
WHQ_NT_DOMAIN\CB083492 <cata_banu98@yahoo.com>
2020-09-18 11:17:41 +0300

    Deleted 'br ' tags from header component

--- src/components/header-component/header-component.component.tsx
+++ src/components/header-component/header-component.component.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import ActionHeader from 'terra-action-header'
 const AnamnesisHeader = () => (
   <div>
-    <br />
     <ActionHeader title=' Anamnesis' />
-    <br />
   </div>
 )
 export default AnamnesisHeader
