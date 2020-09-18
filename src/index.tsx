92e32e2d7be03dc6580ea277fe7fae2ead14ef11
WHQ_NT_DOMAIN\CB083492 <cata_banu98@yahoo.com>
2020-09-18 10:48:05 +0300

    Using specific name for css selector

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
+import styles from './styles.module.css'
 import AnamnesisHeader from './components/header-component/header-component.component'
 import AnamnesisBody from './components/body-component/body-component.component'
 import AnamnesisFooter from './components/footer-component/footer-component.component'
 
 export const Anamnesis = () => {
   return (
-    <div>
+    <div className={styles.anamnesisContainer}>
       <AnamnesisHeader></AnamnesisHeader>
       <AnamnesisBody></AnamnesisBody>
       <AnamnesisFooter></AnamnesisFooter>
